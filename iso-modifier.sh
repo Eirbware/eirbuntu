bf0f1894bd6b5a76b74048ed1ca8a08966726e62
Raphaël Bourgouin <raphael.bourgouin@enseirb-matmeca.fr>
2025-07-17 19:07:35 +0200

    Initial commit

---
+++ iso-modifier.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+VERSION=minimal.fr
+set -x
+#set -e
+
+# Uses ubuntu autoinstall
+
+### Update and install necesary packages
+apt-get update
+apt-get install -y xorriso 
+
+### Creating tmp dir to work in
+TMP_DIR=/iso-modif
+mkdir -p $TMP_DIR
+
+# Extracting iso
+ISO=iso/$(ls iso | head -n 1)
+xorriso -osirrox on -indev $ISO -extract / $TMP_DIR &> /dev/null
+
+### Copying autoinstall yaml
+cp autoinstall.yaml $TMP_DIR
+
+### Copying MBR partition (necessary to boot from usb)
+MBR_FILE=/mbr_copy.bin
+dd if=$ISO bs=1 count=432 of=$MBR_FILE
+
+### Repackaging iso
+NEW_ISO_PATH=/app/new-iso/$(ls /app/iso | head -n 1 | sed 's/ubuntu/eirbuntu/')
+xorriso -as mkisofs -r -V "Eirbuntu" \
+  -o $NEW_ISO_PATH\
+  -J -l -c boot.catalog\
+  -isohybrid-mbr $MBR_FILE \
+  -b boot/grub/i386-pc/eltorito.img\
+  -no-emul-boot -boot-load-size 4 -boot-info-table \
+  -eltorito-alt-boot -e EFI/boot/grubx64.efi \
+  -no-emul-boot -isohybrid-gpt-basdat \
+  $TMP_DIR
+
