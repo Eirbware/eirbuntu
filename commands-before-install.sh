28fe6d2d6e4d563b7aad807995cd00b7eb054721
Raphaël Bourgouin <raphael.bourgouin@enseirb-matmeca.fr>
2025-07-18 18:53:01 +0200

    Forgot to modify ubuntu logo in plymouth

--- commands-before-install.sh
+++ commands-before-install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 ### This file contains the commands that are ran just before the installation
 
-### Adding eirbuntu theme
+### Adding eirbuntu theme and backgound
 cp /cdrom/assets/eirbuntu-theme /usr/share/plymouth/themes/eirbuntu -r
-
-### Adding the eirbuntu backgound 
 cp /cdrom/assets/eirbuntu-background.png /usr/share/backgound
+cp /cdrom/assets/ubunto-logo.png /usr/share/plymouth/ubunto-logo.png
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+gsettings set org.gnome.desktop.backgound picture-uri-dark file:///usr/share/backgound/eirbuntu-background.png
 
 ### Adding desktop files
 cp /cdrom/desktop_files/* ~/Desktop
