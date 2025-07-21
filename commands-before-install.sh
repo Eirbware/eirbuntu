d339a69ec04db941b946d0b75f33e05da14e709b
Raphaël Bourgouin <raphael.bourgouin@enseirb-matmeca.fr>
2025-07-21 18:52:11 +0200

    More typos

--- commands-before-install.sh
+++ commands-before-install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 ### This file contains the commands that are ran just before the installation
 
-### Adding eirbuntu theme and backgound
+### Adding eirbuntu theme and background
 cp /cdrom/assets/eirbuntu-theme /usr/share/plymouth/themes/eirbuntu -r
-cp /cdrom/assets/eirbuntu-background.png /usr/share/backgound
-cp /cdrom/assets/ubunto-logo.png /usr/share/plymouth/ubunto-logo.png
+cp /cdrom/assets/eirbuntu-background.png /usr/share/backgrounds
+cp /cdrom/assets/ubuntu-logo.png /usr/share/plymouth/ubuntu-logo.png
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-gsettings set org.gnome.desktop.backgound picture-uri-dark file:///usr/share/backgound/eirbuntu-background.png
+gsettings set org.gnome.desktop.background picture-uri-dark file:///usr/share/backgrounds/eirbuntu-background.png
+gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/eirbuntu-background.png
 
 ### Adding desktop files
 cp /cdrom/desktop_files/* ~/Desktop
