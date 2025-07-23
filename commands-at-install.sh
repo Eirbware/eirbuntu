480a5f2598b17a44b804235deccebc3afbaa4c4d
Raphaël Bourgouin <raphael.bourgouin@enseirb-matmeca.fr>
2025-07-23 18:01:13 +0200

    Change script to always use the first user (id 1000)

--- commands-at-install.sh
+++ commands-at-install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+ID=1000 #Id of the first user
+USER=$(id -nu $ID)
+
 ### This file contains the commands that are ran just before the installation
 
 ### Unpack and install preinstalled packages
@@ -12,12 +15,12 @@ cp /cdrom/assets/eirbuntu-theme /usr/share/plymouth/themes/eirbuntu -r
 cp /cdrom/assets/eirbuntu-background.png /usr/share/backgrounds
 cp /cdrom/assets/ubuntu-logo.png /usr/share/plymouth/ubuntu-logo.png
 
-XDG_RUNTIME_DIR=/run/user/$(id -u ubuntu)
+XDG_RUNTIME_DIR=/run/user/$ID
 
-sudo -u ubuntu DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus" gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-sudo -u ubuntu DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus" gsettings set org.gnome.desktop.background picture-uri-dark file:///usr/share/backgrounds/eirbuntu-background.png
-sudo -u ubuntu DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus" gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/eirbuntu-background.png
-sudo -u ubuntu DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus" gsettings set org.gnome.desktop.interface gtk-theme eirbuntu
+sudo -u $USER DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus" gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+sudo -u $USER DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus" gsettings set org.gnome.desktop.background picture-uri-dark file:///usr/share/backgrounds/eirbuntu-background.png
+sudo -u $USER DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus" gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/eirbuntu-background.png
+sudo -u $USER DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus" gsettings set org.gnome.desktop.interface gtk-theme eirbuntu
 
 ### Adding desktop files
-cp /cdrom/desktop-files/* /home/ubuntu/Desktop -r
+cp /cdrom/desktop-files/* /home/$USER/Desktop -r
